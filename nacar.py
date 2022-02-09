405939b4342d01b1a08d323f98d90f4d03446756
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-02-09 23:39:18 +0000

    Improve handling of YAML parser errors

--- nacar.py
+++ nacar.py
@@ -9,23 +9,30 @@ from os.path import exists as file_exists
 
 from yaml import safe_load
 from yaml.scanner import ScannerError
+from yaml.parser import ParserError
 
 
 def load_data_from_file(file_path: str) -> dict:
     if not file_exists(file_path):
         raise FileNotFoundError(f"The specified file '{file_path}' does not exist.")
 
+    invalid_yml_message = f"Invalid YAML in '{file_path}'. " \
+                          f"Please provide a blueprint that is valid YAML."
     try:
         with io.open(file_path, 'r') as stream:
             data = safe_load(stream)
         return data
-    except ScannerError as e:
-        raise ScannerError(f"Invalid YAML in '{file_path}'. "
-            "Please provide a blueprint that is valid YAML.")
+    except ScannerError:
+        raise ScannerError(invalid_yml_message)
+    except ParserError:
+        raise ParserError(invalid_yml_message)
 
 
 def main():
-    print(load_data_from_file('yml/blueprint_example.yml'))
+    try:
+        print(load_data_from_file('yml/blueprint_example.yml'))
+    except (FileNotFoundError, ScannerError, ParserError) as e:
+        print(str(e))
 
 
 if __name__ == '__main__':
