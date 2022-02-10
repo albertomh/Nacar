d0faed5a309b57eedd75e2ca1f9b5d85f4360cf3
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-02-10 23:50:51 +0000

    Add Cerberus for schema validation; create Schema module

---
+++ nacar/schema.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Nacar | Schema utilities
+# Copyright 2022 Alberto Morón Hernández
+# [github.com/albertomh/Nacar]
+
+
+class Schema:
+
+    def __init__(self):
+        self.validator = None
