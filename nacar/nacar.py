827a36596c0213307ba20f461455a8a4ff7c8810
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-02-11 21:44:23 +0000

    Add schema validation for blueprints

--- nacar/nacar.py
+++ nacar/nacar.py
@@ -16,20 +16,34 @@ from os.path import abspath
 from yaml.scanner import ScannerError
 
 from file_io import FileIO
+from schema import Schema
 
 
 class Nacar:
-    def __init__(self, file_io):
+    def __init__(self, file_io: FileIO, schema: Schema):
         self.file_io = file_io
+        self.schema = schema
 
     def run(self, blueprint_path):
+        """
+        Read and parse the given blueprint and output a bash script.
+        :param blueprint_path: Path to the YAML blueprint to process.
+        :return:
+        """
+        blueprint: dict
+
         try:
-            blueprint: dict = self.file_io.parse_yml_file(blueprint_path)
-            print(blueprint)  # TODO: remove.
+            blueprint = self.file_io.parse_yml_file(blueprint_path)
         except (FileNotFoundError, ScannerError) as e:
             print(str(e))
             return
 
+        validator = self.schema.get_validator()
+        schema_is_valid: bool = validator.validate(blueprint)
+        if schema_is_valid:
+            print(blueprint)  # TODO: implement
+        else:
+            print("Invalid schema")  # TODO:
 
 def main():
     # Two arguments must be present: `nacar.py` & a path to a blueprint.
@@ -38,7 +52,8 @@ def main():
         return
 
     file_io = FileIO()
-    nacar = Nacar(file_io)
+    schema = Schema()
+    nacar = Nacar(file_io, schema)
 
     blueprint_path = argv[1]
     nacar.run(blueprint_path)
--- nacar/schema.py
+++ nacar/schema.py
@@ -10,8 +10,71 @@ Schema utilities
 TODO: document
 """
 
+from yaml.scanner import ScannerError
+from cerberus import Validator
+
 
 class Schema:
 
     def __init__(self):
         self.validator = None
+        self.add_blueprint_schema_to_validator()
+
+    @staticmethod
+    def get_blueprint_schema() -> dict:
+        schema = {
+            'title': {'type': 'string', 'required': True, 'minlength': 1, 'maxlength': 178},  # noqa
+
+            'meta': {
+                'type': 'dict',
+                'required': False,
+                'schema': {
+                    'width': {'type': 'integer', 'required': False, 'min': 0, 'max': 180}  # noqa
+                }
+            },
+
+            'screens': {
+                'type': 'list',
+                'required': True,
+                'schema': {
+                    'type': 'dict',
+                    'schema': {
+                        'name': {'type': 'string', 'required': True, 'minlength': 1, 'maxlength': 64},      # noqa
+                        'options': {
+                            'type': 'list',
+                            'minlength': 1,
+                            'maxlength': 999,
+                            'schema': {
+                                'type': 'dict',
+                                'anyof_schema': [
+                                    {
+                                        'name': {'type': 'string', 'required': True, 'minlength': 1, 'maxlength': 64},    # noqa
+                                        'link': {'type': 'string', 'required': True, 'minlength': 1, 'maxlength': 64}     # noqa
+                                    },
+                                    {
+                                        'name': {'type': 'string', 'required': True, 'minlength': 1, 'maxlength': 64},    # noqa
+                                        'action': {'type': 'string', 'required': True, 'minlength': 1, 'maxlength': 256}  # noqa
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return schema
+
+    def add_blueprint_schema_to_validator(self):
+        """
+        Pass the main blueprint schema to the Cerberus validator.
+        """
+        try:
+            blueprint_schema: dict = Schema.get_blueprint_schema()
+        except (FileNotFoundError, ScannerError) as e:
+            print(str(e))
+            return
+        self.validator = Validator(blueprint_schema)
+
+    def get_validator(self):
+        return self.validator
