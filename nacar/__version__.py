8dee7b94c4fb7a4edaef4a61371b9b2705b3bbf0
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-02-15 23:20:13 +0000

    Add Nacar version information to generated script

---
+++ nacar/__version__.py
@@ -0,0 +1,16 @@
+"""
+Nacar
+Copyright 2022 Alberto Morón Hernández
+[github.com/albertomh/Nacar]
+
+Version information
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+"""
+
+__title__ = 'Nacar'
+__description__ = 'Create text-based interfaces from YAML blueprints'
+__url__ = 'https://github.com/albertomh/Nacar'
+__version__ = '0.0.1'
+__author__ = 'Alberto Morón Hernández'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Alberto Morón Hernández'
