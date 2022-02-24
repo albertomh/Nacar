a77bf0cea99e3ef4d651e9da82d2e2b739b836ae
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-02-24 20:54:26 +0000

    Make Nacar apps written in bash executable

--- nacar/file_io.py
+++ nacar/file_io.py
@@ -7,9 +7,11 @@ File IO utilities
 ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
 TODO: document
 """
+
 import os
 from os.path import exists as file_exists
 from os.path import abspath
+import stat
 
 from yaml import safe_load
 from yaml.scanner import ScannerError
@@ -47,8 +49,23 @@ class FileIO:
         if target_language == TargetLanguage.BASH:
             with open(file_path, 'w') as outfile:
                 outfile.write(script_content)
+                FileIO.make_file_executable(file_path)
 
         else:
             raise NotImplementedError(f"There is no writer configured for "
                                       f"writing Nacar apps in "
                                       f"{target_language.name.title()}.")
+
+    @staticmethod
+    def make_file_executable(file_path: str) -> None:
+        """
+        Set the given file's permissions to 0775 - executable by everyone.
+        [stackoverflow.com/a/55590988]
+        """
+        if not file_exists(file_path):
+            raise FileNotFoundError(f"Could not find '{abspath(file_path)}' when attempting to make this file executable.")  # noqa
+
+        current_mode = os.stat(file_path).st_mode
+        # Bitwise OR current mode with executable modes (user, group, others).
+        new_mode = current_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+        os.chmod(file_path, new_mode)
