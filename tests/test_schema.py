bd58a5d756301b145b54d22a29a62e0aa18fdb06
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-03-18 22:51:38 +0000

    Test adding subschemas to Cerberus registry

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -7,6 +7,10 @@
 # Test adding subschemas to the registry, setting missing optional attributes,
 # schema property getters, and raising custom InvalidSchemaErrors.
 
+from cerberus import schema_registry
+
+from nacar.schema import Schema
+
 
 def cerberus_registry_contains_expected_subschemas(registry) -> bool:
     schemas_in_registry_count = len(registry.all().keys())
@@ -21,3 +25,15 @@ def cerberus_registry_contains_expected_subschemas(registry) -> bool:
 
     return (schemas_in_registry_count == len(expected_subschema_names)
             and (expected_subschema_names == actual_subschema_keys))
+
+
+def test_instantiating_schema_adds_subschemas_to_cerberus() -> None:
+    schema_registry.clear()
+    Schema()
+    assert cerberus_registry_contains_expected_subschemas(schema_registry) is True
+
+
+def test_add_blueprint_subschemas_to_registry():
+    schema_registry.clear()
+    Schema.add_blueprint_subschemas_to_registry()
+    assert cerberus_registry_contains_expected_subschemas(schema_registry) is True
